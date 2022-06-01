7360498feab96537d076617d5f438c0024fedb4a
Kivinsae Fang <kivinsae@kivinsae.com>
2022-06-01 18:07:13 +0800

    add(post): title: An ugly feature about io.EOF in Go

--- public_sync.sh
+++ public_sync.sh
@@ -7,7 +7,7 @@ exec >> /var/log/nginx/syntax_check.log 2>&1
 echo "============================================"
 echo $(date +"%F %T")
 
-/usr/bin/hexo clean		# Clear the existing public files
+/usr/bin/hexo clean		    # Clear the existing public files
 /usr/bin/hexo generate		# Generate new version of public files
 systemctl restart hexo		# Only renew the local hexo server. For PoC
 
