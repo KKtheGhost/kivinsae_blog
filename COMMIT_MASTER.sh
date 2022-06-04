94575162be8fcc262cce06364159eca51e276834
KIVINSAEHOME\Kivinsae <kivinsae@gmail.com>
2022-06-04 21:56:14 +0800

    add(Script): Add and modify the COMMIT_MASTER script for Windows.

--- COMMIT_MASTER.sh
+++ COMMIT_MASTER.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Please input commit info in AngularJS form (<type>(topic): details):"
-read commit
+read -p "Enter the type of commit" type
+read -p "Enter the topic of commit" topic
+read -p "Enter the details of commit" details
 git add .
-git commit -m "${commit}"
+git commit -m "$type($topic): $details"
 git push git@github.com:KKtheGhost/kivinsae_blog.git master
