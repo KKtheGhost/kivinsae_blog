052a2d7952e755414ecbfdeef7702f46306891e6
Kivinsae Fang <kivinsae@kivinsae.com>
2022-06-05 12:59:44 +0800

    fix(permission): Fix a chmod problem caused by Windows

--- COMMIT_MASTER.sh
+++ COMMIT_MASTER.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please input commit info in AngularJS form (<type>(topic): details):"
-read -p "Enter the type of commit" type
-read -p "Enter the topic of commit" topic
-read -p "Enter the details of commit" details
+read -p "Enter the type of commit:" type
+read -p "Enter the topic of commit:" topic
+read -p "Enter the details of commit:" details
 git add .
 git commit -m "$type($topic): $details"
 git push git@github.com:KKtheGhost/kivinsae_blog.git master
