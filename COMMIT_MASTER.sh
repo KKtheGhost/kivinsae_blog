bbd1e20d67c233b710664876c165327f33bb2a3b
KIVINSAEHOME\Kivinsae <kivinsae@gmail.com>
2022-06-08 01:38:57 +0800

    modify(action): Add step 'Make the public_sync script could be executed.'

--- COMMIT_MASTER.sh
+++ COMMIT_MASTER.sh
@@ -4,7 +4,6 @@ echo "Please input commit info in AngularJS form (<type>(topic): details):"
 read -p "Enter the type of commit:" type
 read -p "Enter the topic of commit:" topic
 read -p "Enter the details of commit:" details
-chmod +x ./*.sh
 git add .
 git commit -m "$type($topic): $details"
 git push git@github.com:KKtheGhost/kivinsae_blog.git master
