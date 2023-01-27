8a6a8dcb907b7dce70f202f4388cafc053ff9d8c
KKtheGhost <kivinsae@gmail.com>
2023-01-27 16:13:52 +0800

    Modify: Change the target OSS to store static assets.

--- public_sync.sh
+++ public_sync.sh
@@ -41,4 +41,4 @@ nginx -t
 nginx -s reload
 
 # Ossutil sync to www.kivinsae.com
-ossutil64 sync /var/www/hexo/public/ oss://kivinsae-com-oss/ --delete --force
+ossutil64 sync /var/www/hexo/public/ oss://kivinsae-com-web/ --delete --force
